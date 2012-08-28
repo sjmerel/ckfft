eed6748970f900aa5e5a54df35d6f1929265c6fc
steve <steve@crickettechnology.com>
2012-08-28 18:51:30 +0000

    add android ckfft
    git-svn-id: file:///Users/steve/svn/ckfft/trunk@7 743b922c-e3f2-40ca-8674-e6e14afc19ac

---
+++ src/ckfft/android/jni/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ckfft
+LOCAL_SRC_FILES := ../../ckfft.cpp
+#LOCAL_CFLAGS += -Wno-psabi # fix warning about va_args in ndk r8b
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../inc 
+
+include $(BUILD_STATIC_LIBRARY)
+
+
---
+++ src/ckfft/android/jni/Application.mk
@@ -0,0 +1,2 @@
+APP_MODULES := ckfft
+APP_ABI := armeabi armeabi-v7a
