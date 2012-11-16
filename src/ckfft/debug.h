30d5d96efaf2ecb05c02f77da25117ba91311240
steve <steve@crickettechnology.com>
2012-11-16 17:04:09 +0000

    debug tweaks
    git-svn-id: file:///Users/steve/svn/ckfft/trunk@40 743b922c-e3f2-40ca-8674-e6e14afc19ac

--- src/ckfft/debug.h
+++ src/ckfft/debug.h
@@ -1,38 +1,10 @@
 #pragma once
 #include "ckfft/platform.h"
-#include <stdio.h>
-
-#if CKFFT_PLATFORM_ANDROID
-#  include <android/log.h>
-#elif CKFFT_PLATFORM_WIN
-#  include <stdarg.h>
-#  include <windows.h>
-#endif
 
 
-////////////////////////////////////////
-// print macro
+void CkDebugPrintf(const char* fmt, ...);
 
-#if CKFFT_PLATFORM_ANDROID
-#  define CKFFT_PRINTF(fmt, ...) __android_log_print(ANDROID_LOG_INFO, "CKFFT", fmt, ##__VA_ARGS__)
-#elif CKFFT_PLATFORM_WIN
-#  define CKFFT_PRINTF(fmt, ...) debugPrintf(fmt, __VA_ARGS__)
-#else
-#  define CKFFT_PRINTF printf
-#endif
+#define CKFFT_PRINTF(fmt, ...) CkDebugPrintf(fmt, __VA_ARGS__)
 
 
-#if CKFFT_PLATFORM_WIN
-void debugPrintf(const char* fmt, ...)
-{
-    char buf[4096];
-    va_list args;
-    va_start(args, fmt);
-    int n = _vsnprintf_s(buf, sizeof(buf), sizeof(buf)-1, fmt, args);
-    va_end(args);
 
-    buf[n] = '\0';
-    OutputDebugString(buf);
-    printf(buf);
-}
-#endif
