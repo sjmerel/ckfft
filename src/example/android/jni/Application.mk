e0e1d040219505eb186f653901e9ed352345f204
steve <steve@crickettechnology.com>
2013-09-20 15:59:56 +0000

    update for xcode 5; notes to build with clang
    git-svn-id: file:///Users/steve/svn/ckfft/trunk@73 743b922c-e3f2-40ca-8674-e6e14afc19ac

--- src/example/android/jni/Application.mk
+++ src/example/android/jni/Application.mk
@@ -1,2 +1,5 @@
 APP_ABI := armeabi armeabi-v7a
 
+# it is highly recommended that you compile the CKFFT sources with clang instead of gcc, as clang produces much more efficient NEON code from intrinsics.
+NDK_TOOLCHAIN_VERSION := clang
+
--- src/test/android/jni/Application.mk
+++ src/test/android/jni/Application.mk
@@ -1,4 +1,5 @@
 APP_STL := stlport_static
 APP_ABI := armeabi armeabi-v7a
 APP_OPTIM := release
+NDK_TOOLCHAIN_VERSION := clang
 
